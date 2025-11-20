b713a7e616c1fe0fdc5c6d25396ad4f1fbf2e45b
Leandro Nuñez <84481382+lean-nunez@users.noreply.github.com>
2025-11-20 18:06:26 -0300

    Delete Base de Datos directory

--- Base de Datos/llave.sql
+++
@@ -1,9 +0,0 @@
--- Tags RFID
-CREATE TABLE rfid_tags (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  tag_uid VARCHAR(100) NOT NULL UNIQUE, -- el UID que devuelve el lector (ej "04A3B2C1")
-  user_id INT NULL,
-  description VARCHAR(200),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
-);
